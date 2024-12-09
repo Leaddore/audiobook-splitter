fcb4f1a08cf97cc3cc61c83e113ddb9c48bd6f7c
Leaddore <Leaddore@users.noreply.github.com>
2024-12-08 23:12:39 -0500

    Create Range.java
    Added range class to store the start and stop times to split the audiobook into chapters

---
+++ src/main/java/com/leaddore/audiobook/splitter/range/Range.java
@@ -0,0 +1,25 @@
+package com.leaddore.audiobook.splitter.range;
+
+public class Range {
+
+	private String startTime;
+
+	private String stopTime;
+
+	public String getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(String startTime) {
+		this.startTime = startTime;
+	}
+
+	public String getStopTime() {
+		return stopTime;
+	}
+
+	public void setStopTime(String stopTime) {
+		this.stopTime = stopTime;
+	}
+
+}
