280bc2a45cbee2798bacc52bf1752ea8a3706e69
Leaddore <Leaddore@users.noreply.github.com>
2024-12-19 23:35:37 -0500

    Create FileUtils.java
    Added file utils to create and read timecodes from file

---
+++ src/main/java/com/leaddore/audiobook/utils/FileUtils.java
@@ -0,0 +1,90 @@
+package com.leaddore.audiobook.utils;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.leaddore.audiobook.splitter.range.Range;
+
+/**
+ * The Class FileUtils.
+ */
+public final class FileUtils {
+
+	/** The Constant LOGGER. */
+	private static final Logger LOGGER = LogManager.getLogger(FileUtils.class);
+
+	/** The Constant OUTPUT_FILE_NAME. */
+	public static final String OUTPUT_FILE_NAME = "timecodes";
+
+	/**
+	 * Instantiates a new file utils.
+	 */
+	private FileUtils() {
+
+		// Static class, no need for constructing.
+
+	}
+
+	/**
+	 * Write timecode file.
+	 *
+	 * @param ranges the ranges
+	 */
+	public static final void writeTimecodeFile(List<Range> ranges) {
+
+		StringBuilder sb = new StringBuilder();
+
+		ranges.stream().forEach(s -> sb.append(s.getStartTime()).append("-").append(s.getStopTime()).append("\n"));
+
+		try (BufferedWriter writer = new BufferedWriter(new FileWriter(OUTPUT_FILE_NAME))) {
+
+			writer.write(sb.toString());
+
+		} catch (IOException e) {
+
+			LOGGER.error("Unable to write time codes to file {}", OUTPUT_FILE_NAME);
+			e.printStackTrace();
+		}
+
+	}
+
+	/**
+	 * Read ranges file.
+	 *
+	 * @return the list
+	 */
+	public static final List<Range> readRangesFile() {
+
+		List<Range> ranges = new ArrayList<>();
+
+		try (BufferedReader reader = new BufferedReader(new FileReader(OUTPUT_FILE_NAME))) {
+
+			ranges = reader.lines().map(line -> {
+				String[] fields = line.split("-");
+				Range r = new Range();
+				r.setStartTime(fields[0]);
+				r.setStopTime(fields[1]);
+				return r;
+			}).collect(Collectors.toList());
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return ranges;
+
+	}
+
+}
