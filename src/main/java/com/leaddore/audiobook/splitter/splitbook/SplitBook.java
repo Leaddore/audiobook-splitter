7a6bea5f6f3c8d8047c02985bdb52e027045e459
Leaddore <Leaddore@users.noreply.github.com>
2024-12-20 23:55:18 -0500

    Update SplitBook.java
    Added comments and some cleanup

--- src/main/java/com/leaddore/audiobook/splitter/splitbook/SplitBook.java
+++ src/main/java/com/leaddore/audiobook/splitter/splitbook/SplitBook.java
@@ -1,21 +1,38 @@
 package com.leaddore.audiobook.splitter.splitbook;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.leaddore.audiobook.splitter.range.Range;
 
+/**
+ * The Class SplitBook.
+ */
 public class SplitBook {
 
+	/** The Constant LOGGER. */
+	private static final Logger LOGGER = LogManager.getLogger(SplitBook.class);
+
+	/** The range. */
 	private List<Range> range;
 
+	/** The audio book path. */
 	private String audioBookPath = "";
 
+	/** The ffmpeg path. */
 	private String ffmpegPath = "";
 
+	/**
+	 * Instantiates a new split book.
+	 *
+	 * @param range         the range
+	 * @param audioBookPath the audio book path
+	 * @param ffmpegPath    the ffmpeg path
+	 */
 	public SplitBook(List<Range> range, String audioBookPath, String ffmpegPath) {
 
 		setRange(range);
@@ -24,14 +41,27 @@ public class SplitBook {
 
 	}
 
+	/**
+	 * Gets the range.
+	 *
+	 * @return the range
+	 */
 	public List<Range> getRange() {
 		return range;
 	}
 
+	/**
+	 * Sets the range.
+	 *
+	 * @param range the new range
+	 */
 	public void setRange(List<Range> range) {
 		this.range = range;
 	}
 
+	/**
+	 * Split it up.
+	 */
 	public void splitItUp() {
 
 		for (int i = 0; i < range.size(); i++) {
@@ -44,32 +74,40 @@ public class SplitBook {
 
 	}
 
+	/**
+	 * Split mp 3.
+	 *
+	 * @param timeRange the time range
+	 * @param i         the i
+	 */
 	private void splitMp3(Range timeRange, int i) {
 
 		List<String> command = generateCommand(timeRange, i);
 
 		ProcessBuilder pb = new ProcessBuilder(command);
+		pb.redirectErrorStream(true);
 
 		try {
 			Process process = pb.start();
-
-			BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-			String line;
-			while ((line = reader.readLine()) != null) {
-				System.out.println(line);
-			}
 			process.waitFor();
-			System.out.println("Segment created: " + audioBookPath.split("\\.")[0] + i + ".mp3");
+
+			LOGGER.warn("Segment Created: {}", audioBookPath.split("\\.")[0] + i + ".mp3");
 
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
 	}
 
+	/**
+	 * Generate command.
+	 *
+	 * @param timeRange the time range
+	 * @param i         the i
+	 * @return the list
+	 */
 	private List<String> generateCommand(Range timeRange, int i) {
 
 		List<String> command = new ArrayList<>();
@@ -87,18 +125,38 @@ public class SplitBook {
 		return command;
 	}
 
+	/**
+	 * Gets the audio book path.
+	 *
+	 * @return the audio book path
+	 */
 	public String getAudioBookPath() {
 		return audioBookPath;
 	}
 
+	/**
+	 * Sets the audio book path.
+	 *
+	 * @param audioBookPath the new audio book path
+	 */
 	public void setAudioBookPath(String audioBookPath) {
 		this.audioBookPath = audioBookPath;
 	}
 
+	/**
+	 * Gets the ffmpeg path.
+	 *
+	 * @return the ffmpeg path
+	 */
 	public String getFfmpegPath() {
 		return ffmpegPath;
 	}
 
+	/**
+	 * Sets the ffmpeg path.
+	 *
+	 * @param ffmpegPath the new ffmpeg path
+	 */
 	public void setFfmpegPath(String ffmpegPath) {
 		this.ffmpegPath = ffmpegPath;
 	}
